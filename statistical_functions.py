f08c979e6feacf5ea33c25cde846e7ff8be1e221
chervinsky92 <appdesigner2192@gmail.com>
2020-09-25 09:43:21 -0400

    Coin flip simulations

--- statistical_functions.py
+++ statistical_functions.py
@@ -1,4 +1,5 @@
 from math import sqrt
+import random
 
 # from statistics import mean
 def mean(data):
@@ -43,10 +44,20 @@ def incremental_mean(oldmean, n, x):
 # dist: dictionary of results and their corresponding probabilities
 # data: string/list of desired sequence
 def likelihood(dist, data):
-    likely = 1.0
+    l = 1.0
     for item in data:
-        likely *= dist[item]
-    return likely
-
+        l *= dist[item]
+    return l
 # print(likelihood({'A':0.2,'B':0.2,'C':0.2,'D':0.2,'E':0.2}, 'ABCEDDECAB'))
 # print(likelihood({'Good':0.6,'Bad':0.2,'Indifferent':0.2}, ['Good','Bad','Indifferent','Good','Good','Bad']))
+
+# simulate flipping N fair coins
+# int(True) -> 1 
+# int(False) -> 0
+def flip(N):
+    return [random.random() > 0.5 for x in range(N)]
+
+# simulate sets of coin flips
+def sample(N_flips, N_sets):
+    return [mean(flip(N_flips)) for x in range(N_sets)]
+# print(sample(1000, 1000))
